dd72d9868a989b13777da97ded1ae4b396bf1373
mr_kim <rofalfl42@gmail.com>
2022-08-03 23:57:57 +0900

    **프로젝트** : AwsIotCoreMqttApp **작성자** : illi **수정내역**: 1. Mqtt publish & subscribe UseCase 추가

---
+++ app/src/main/java/com/illis/awsiotcoremqtt/domain/usecase/PublishMqttUseCase.kt
@@ -0,0 +1,34 @@
+package com.illis.awsiotcoremqtt.domain.usecase
+
+import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import software.amazon.awssdk.crt.mqtt.MqttClientConnection
+import software.amazon.awssdk.crt.mqtt.MqttMessage
+import software.amazon.awssdk.crt.mqtt.QualityOfService
+import java.util.concurrent.CompletableFuture
+
+class PublishMqttUseCase(val context: ApplicationContext,
+                         private val mqttClient: MqttClientConnection) {
+
+    enum class MqttType(
+        val typeName: String
+    ) {
+        KEYCODE("keycode"),
+        KEYPAD("keypad"),
+        HOTKEY("hotkey")
+    }
+    
+    fun publish1s(msg: String, type: MqttType) : CompletableFuture<Int> {
+        return publish(msg, type, delay = 1)
+    }
+
+    fun publish4s(msg: String, type: MqttType) : CompletableFuture<Int> {
+        return publish(msg, type, delay = 4)
+    }
+
+    private fun publish(msg : String, type: MqttType, delay: Int) : CompletableFuture<Int> {
+
+        val mqttMessage = MqttMessage("topic", msg.toByteArray(), QualityOfService.AT_LEAST_ONCE, false)
+        return mqttClient.publish(mqttMessage)
+    }
+}
---
+++ app/src/main/java/com/illis/awsiotcoremqtt/domain/usecase/SubscribeMqttUseCase.kt
@@ -0,0 +1,30 @@
+package com.illis.awsiotcoremqtt.domain.usecase
+
+import android.content.SharedPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import software.amazon.awssdk.crt.mqtt.MqttClientConnection
+import software.amazon.awssdk.crt.mqtt.QualityOfService
+import java.nio.charset.StandardCharsets
+import java.util.concurrent.CompletableFuture
+
+class SubscribeMqttUseCase(val context: ApplicationContext,
+                           private val mqttClient: MqttClientConnection) {
+
+    fun subscribeTVCommon() : CompletableFuture<String> {
+        val future = CompletableFuture<String>()
+        mqttClient.subscribe("topic", QualityOfService.AT_LEAST_ONCE) { message ->
+            val msg = String(message.payload, StandardCharsets.UTF_8)
+            future.complete(msg)
+        }
+        return future
+    }
+
+    fun subscribeResponse() : CompletableFuture<String> {
+        val future = CompletableFuture<String>()
+        mqttClient.subscribe("topic", QualityOfService.AT_LEAST_ONCE) { message ->
+            val msg = String(message.payload, StandardCharsets.UTF_8)
+            future.complete(msg)
+        }
+        return future
+    }
+}
