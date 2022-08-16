50d47bbaba7424a15b4a3132807c142922d31805
mr_kim <rofalfl42@gmail.com>
2022-08-16 23:50:49 +0900

    **수정내역**: 1. MqttViewModel subscribe 로직 수정, FactoryModule 패키지 수정 **프로젝트** : AwsIotCoreMqttApp **작성자** : illi

--- app/src/main/java/com/illis/awsiotcoremqtt/presentation/di/FactoryModule.kt
+++ app/src/main/java/com/illis/awsiotcoremqtt/presentation/di/FactoryModule.kt
@@ -1,6 +1,8 @@
-package com.illis.awsiotcoremqtt.domain.usecase
+package com.illis.awsiotcoremqtt.presentation.di
 
 import android.app.Application
+import com.illis.awsiotcoremqtt.domain.usecase.PublishMqttUseCase
+import com.illis.awsiotcoremqtt.domain.usecase.SubscribeMqttUseCase
 import com.illis.awsiotcoremqtt.presentation.viewmodel.mqtt.MqttViewModelFactory
 import dagger.Module
 import dagger.Provides
--- app/src/main/java/com/illis/awsiotcoremqtt/presentation/viewmodel/mqtt/MqttViewModel.kt
+++ app/src/main/java/com/illis/awsiotcoremqtt/presentation/viewmodel/mqtt/MqttViewModel.kt
@@ -1,55 +1,44 @@
 package com.illis.awsiotcoremqtt.presentation.viewmodel.mqtt
 
 import android.app.Application
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.illis.awsiotcoremqtt.domain.usecase.PublishMqttUseCase
 import com.illis.awsiotcoremqtt.domain.usecase.SubscribeMqttUseCase
-import com.orhanobut.logger.Logger
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.concurrent.TimeUnit
 
-data class MqttState(
-    val isLoading: Boolean = false,
-    val completedTask: String = ""
-)
+enum class MqttState {
+    MQTT_COMPLETE,
+    MQTT_FAIL
+}
 
 class MqttViewModel(
     app : Application,
     private val subscribeMqttUseCase: SubscribeMqttUseCase,
     private val publishMqttUseCase: PublishMqttUseCase
 ) : AndroidViewModel(app) {
+    val mqttStatus : MutableLiveData<MqttState> = MutableLiveData()
 
-    fun publish(msg : String, type: PublishMqttUseCase.MqttType) = viewModelScope.launch(Dispatchers.IO) {
-        publishMqttUseCase.publish1s(msg, type).thenAcceptAsync {
-            Logger.d(subscribeMqttUseCase.subscribeResponse().get())
-        }
+    private fun publish(msg : String, type: PublishMqttUseCase.MqttType, delay: Int) = viewModelScope.launch(Dispatchers.IO) {
+        publishMqttUseCase.publish(msg, type, delay).get()
+        subscribe()
     }
 
-    private fun isNetworkAvailable(context: Context?) : Boolean {
-        val connectivityManager = (context as Context).getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            var res = false
-            connectivityManager.let {
-                it.getNetworkCapabilities(connectivityManager.activeNetwork)?.apply {
-                    res = when {
-                        hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-                        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-                        hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-                        else -> false
-                    }}}
-            return res
-        } else {
-            val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
-                return true
+    private fun subscribe() = viewModelScope.launch(Dispatchers.IO) {
+        val subscribe = subscribeMqttUseCase.subscribeResponse()
+        try {
+            val result = subscribe.get(7, TimeUnit.SECONDS)
+            if (result == "ok")
+                mqttStatus.postValue(MqttState.MQTT_COMPLETE)
+            else {
+                mqttStatus.postValue(MqttState.MQTT_FAIL)
             }
+        } catch (e: Exception) {
+            mqttStatus.postValue(MqttState.MQTT_FAIL)
         }
-        return false
     }
 
 }
